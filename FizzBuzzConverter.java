de6582048cfb7b7ed7dd681307d35d0e00be144d
Erin Stevenson <estevenson_94@live.com>
2019-02-22 19:42:54 +0000

    Passed test to return Fizz when divisable by 3

--- FizzBuzzConverter.java
+++ FizzBuzzConverter.java
@@ -1,5 +1,8 @@
 public class FizzBuzzConverter {
     public String convert(int number) {
+        if(number%3==0) {
+            return "Fizz";
+        }
         return String.valueOf(number);
     }
 }
